2d12937f6372bb0177094c8f769effe56d83651d
santygast <santinogastaldi18@gmail.com>
2023-08-17 23:23:48 -0300

    agrega un comentario

--- Coderhouse/Program.cs
+++ Coderhouse/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+cjl
+    n nn
+m
+
+git init
+git status
+    kmpek
+    v ee
+      ecfe
+    ee
+console.weiteline
+
+
